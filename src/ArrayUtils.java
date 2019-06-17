dcef74fef293fa881dd781fc6f5629c5788d6400
Artur Przyjemski <spinezpl@gmail.com>
2019-06-17 13:10:56 +0200

    second commit new creation method

--- src/ArrayUtils.java
+++ src/ArrayUtils.java
@@ -1,8 +1,26 @@
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Scanner;
 
 public class ArrayUtils {
 
+    ArrayList creation() {
+        Scanner scan = new Scanner(System.in);
+        ArrayList<Integer> numbers = new ArrayList<>();
+        Integer number = 0;
+        do {
+            System.out.println("Podaj kilka liczb całkowitych większych od zera, dodanie liczby mniejszej od 0 kończy podawanie liczb  ");
+            number = scan.nextInt();
+            if (number >= 0) {
+                numbers.add(number);
+            }
+        }
+        while (number >= 0);
+        {
+            System.out.println("Podałeś liczbę mniejszą od zera");
+        }
+        return numbers ;
+    }
 
     void reversedArray(ArrayList numb) {
         int j = numb.size();
@@ -11,26 +29,26 @@ public class ArrayUtils {
             j--;
         }
     }
-    void sumArray(ArrayList numb){
+
+    void sumArray(ArrayList<Integer> numb) {
         int sum = 0;
-         {for (int i = 0; i <numb.size() ; i++)
-            sum = sum + (int)numb.get(i);
-             for (int i = 0; i <numb.size()-1 ; i++)
-            System.out.print(((int)numb.get(i))+"+");
+        {
+            for (int i = 0; i < numb.size(); i++)
+                sum = sum + numb.get(i);
+            for (int i = 0; i < numb.size() - 1; i++)
+                System.out.print((numb.get(i)) + "+");
         }
-        System.out.print(numb.get(numb.size()-1));
-        System.out.println(" = "+sum);
+        System.out.print((int)numb.get(numb.size() - 1));
+        System.out.println(" = " + sum);
     }
 
-    void smallest(ArrayList numb){
+    void smallest(ArrayList<Integer> numb) {
         System.out.println(Collections.min(numb));
     }
 
-    void largest(ArrayList numb){
+    void largest(ArrayList<Integer> numb) {
         System.out.println(Collections.max(numb));
     }
-
-
 
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,14 @@
 import java.util.ArrayList;
-import java.util.Scanner;
+
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        ArrayList<Integer> numbers = new ArrayList<>();
+
+
         ArrayUtils util1 = new ArrayUtils();
+        ArrayList<Integer> numbers = new ArrayList<>(util1.creation());
 
-        Integer number = 0;
 
-        do {
-            System.out.println("Podaj kilka liczb całkowitych większych od zera, dodanie liczby mniejszej od 0 kończy podawanie liczb  ");
-            number = scan.nextInt();
-            if (number >= 0) {
-                numbers.add(number);
-            }
-        }
-        while (number >= 0);
-        {
-            System.out.println("Podałeś liczbę mniejszą od zera");
-        }
         // System.out.println(numbers);
         util1.reversedArray(numbers);
         System.out.println();
